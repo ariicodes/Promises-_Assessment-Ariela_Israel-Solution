96ca2419bb996e943b98e743146ad0463d677f16
Ariela <arielaisrael12@gmail.com>
2023-08-23 22:44:34 -0400

    fullSession function refactored

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ function ask(question) {
 // Returns: Promise that resolves to a string or an error message
 // TODO: Implement the getFortune function by utilizing the ask function to get the fortune for the question
 // Hint: Call the ask function with the question and extract the fortune from the response array
+
 function getFortune(question) {
 	return ask(question).catch(error => `There was an error: ${error}`);
 }
@@ -22,36 +23,21 @@ function getFortune(question) {
 // TODO: Create a full session by combining the welcome, getFortune, and goodbye functions
 // Hint: Use promise chaining to call the functions in the correct order and concatenate the results
 function fullSession(question) {
-	let welcomeMessage = '';
 	return welcome()
-		.then( message => {
-			welcomeMessage = message
-			if (!question) {
-				throw new Error('A question is required...');
-			}
-			return getFortune(question);
-		})
-		.then(fortune => {
-			return goodbye().then(response => {
-				const sessionArr = [
-					welcomeMessage,
-					`Your question was: ${question}`,
-					`Your fortune is: ${fortune}`,
-					response,
-				];
-				console.log(sessionArr);
-				return sessionArr;
-			});
-		})
-		.catch(error => {
-			const sessionArr = [
-				"Provide me a question and I'll give you an answer...",
-				`There was an error: ${error.message}`,
-				'Best of luck in the future...',
-			];
-			console.log(sessionArr);
-			return sessionArr;
-		});
+		.then(welcomeMess =>
+			ask(question).then(res =>
+				goodbye().then(goodbyeMess => [welcomeMess, ...res, goodbyeMess])
+			)
+		)
+		.catch(err =>
+			welcome().then(welcomeMess =>
+				goodbye().then(goodbyeMess => [
+					welcomeMess,
+					`There was an error: ${err}`,
+					goodbyeMess,
+				])
+			)
+		);
 
 	// Call the welcome function
 	// Chain the getFortune function to get the fortune for the question
